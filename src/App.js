cc7ea108db7c8a692c5872731a7af472bd525d6b
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-01-03 20:37:16 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
-import React from 'react';
-import './App.css';
-import Field from './field';
-import {initialCreate, create} from './logic/create';
-import checkResult from './logic/checkResult';
-import Header from './header';
-import checkLose from './logic/checkLose';
-import LoseModal from './loseModal';
+import React from "react";
+import "./App.css";
+import Field from "./field";
+import { initialCreate, create } from "./logic/create";
+import checkResult from "./logic/checkResult";
+import Header from "./header";
+import checkLose from "./logic/checkLose";
+import LoseModal from "./loseModal";
 
 // react-swipeable
-import { useSwipeable, Swipeable } from 'react-swipeable'
+import { useSwipeable, Swipeable } from "react-swipeable";
+
+// hello world(test)...
 
 const config = {
-  delta: 10,                             // min distance(px) before a swipe starts
-  preventDefaultTouchmoveEvent: true,   // preventDefault on touchmove, *See Details*
-  trackTouch: true,                      // track touch input
-  trackMouse: false,                     // track mouse input
-  rotationAngle: 0,                      // set a rotation angle
-}
+  delta: 10, // min distance(px) before a swipe starts
+  preventDefaultTouchmoveEvent: true, // preventDefault on touchmove, *See Details*
+  trackTouch: true, // track touch input
+  trackMouse: false, // track mouse input
+  rotationAngle: 0 // set a rotation angle
+};
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       map: initialCreate(),
       mapChange: false,
-      loseGame: false,
+      loseGame: false
     };
   }
 
-  handleMove = (e) => {
-
+  handleMove = e => {
     let map = this.state.map,
-        newMap,
-        direction;
+      newMap,
+      direction;
 
-    if(e.keyCode === 37 || e.dir === 'Left') direction = 'left';
-    if(e.keyCode === 38 || e.dir === 'Up') direction = 'top';
-    if(e.keyCode === 39 || e.dir === 'Right') direction = 'right';
-    if(e.keyCode === 40 || e.dir === 'Down') direction = 'bottom';
+    if (e.keyCode === 37 || e.dir === "Left") direction = "left";
+    if (e.keyCode === 38 || e.dir === "Up") direction = "top";
+    if (e.keyCode === 39 || e.dir === "Right") direction = "right";
+    if (e.keyCode === 40 || e.dir === "Down") direction = "bottom";
 
-    switch(direction){
-
-      case 'left':
-        newMap = checkResult(direction , map);
+    switch (direction) {
+      case "left":
+        newMap = checkResult(direction, map);
         break;
-      case 'top':
-        newMap = checkResult(direction , map);
+      case "top":
+        newMap = checkResult(direction, map);
         break;
-      case 'right':
-        newMap = checkResult(direction , map);
+      case "right":
+        newMap = checkResult(direction, map);
         break;
-      case 'bottom':
-        newMap = checkResult(direction , map);
+      case "bottom":
+        newMap = checkResult(direction, map);
         break;
       default:
         return;
@@ -63,53 +63,45 @@ class App extends React.Component {
       mapChange: newMap.mapChange
     });
 
-    if(this.state.mapChange){
-
-      this.setState({map:create(this.state.map)});
+    if (this.state.mapChange) {
+      this.setState({ map: create(this.state.map) });
 
       // check lose
-      if(this.state.map.length === 16){
-        this.setState({loseGame: checkLose(this.state.map)}); // check lose!
+      if (this.state.map.length === 16) {
+        this.setState({ loseGame: checkLose(this.state.map) }); // check lose!
       }
     }
+  };
+
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleMove);
   }
 
-  componentWillMount(){
-    document.addEventListener('keydown', this.handleMove);
-  }
-
-  componentWillUnmount(){
-    document.removeEventListener('keydown', this.handleMove);
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleMove);
   }
 
   // new game reloader
 
   newGame = () => {
     this.setState({
-      map:initialCreate(),
+      map: initialCreate(),
       loseGame: false
     });
-  }
+  };
 
-  render(){
+  render() {
     return (
-      <Swipeable onSwiped={(eventData) => this.handleMove(eventData)} {...config} >
-        <div className='app-wrap'>
-          <Header 
-            score = {this.state.map}
-            handleNewGame = {this.newGame}
-          />
-          <Field 
-            map={this.state.map}
-          />
+      <Swipeable onSwiped={eventData => this.handleMove(eventData)} {...config}>
+        <div className="app-wrap">
+          <Header score={this.state.map} handleNewGame={this.newGame} />
+          <Field map={this.state.map} />
 
-          { 
-            // lose modal... ;)
-            this.state.loseGame && <LoseModal tryAgain={this.newGame}/>
-          }
+          {// lose modal... ;)
+          this.state.loseGame && <LoseModal tryAgain={this.newGame} />}
         </div>
       </Swipeable>
-    )
+    );
   }
 }
 

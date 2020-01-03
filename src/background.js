e24bdcddb1244539fc434f05623c247163908cb1
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-01-03 20:46:42 +0300

    test v2

--- src/background.js
+++ src/background.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const backGround = Array.from(new Array(16));
 
-class BackGround extends React.Component{
-
-	render(){
-		return [
-			backGround.map( (item, index) => {
-				return (<div
-					className = 'map-cell'
-					key = {index}
-				/>)
-			})
-		]
-	}
+class BackGround extends React.Component {
+  render() {
+    return [
+      backGround.map((item, index) => {
+        return <div className="map-cell" key={index} />;
+      })
+    ];
+  }
 }
 
 export default BackGround;
